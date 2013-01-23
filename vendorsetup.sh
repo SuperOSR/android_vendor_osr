60b640421d6d5e707a642090cbffddac26971804
superatmel <superatmel@gmail.com>
2013-01-23 13:31:06 +0100

    Delete devices not suported
    Change-Id: Ifb2465f8d48ccc58f5945daaa0725c19eb22845c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,15 +9,6 @@ else
 fi
 
 # Devices supported
-add_lunch_combo osr_dragon-userdebug
-add_lunch_combo osr_galaxysmtd-userdebug
-add_lunch_combo osr_grouper-userdebug
-add_lunch_combo osr_hackberry-userdebug
 add_lunch_combo osr_i9100-userdebug
 add_lunch_combo osr_i9300-userdebug
-add_lunch_combo osr_mk802-userdebug
-add_lunch_combo osr_n7000-userdebug
-add_lunch_combo osr_pyramid-userdebug
-add_lunch_combo osr_tf201-userdebug
-add_lunch_combo osr_bravo-eng
 
