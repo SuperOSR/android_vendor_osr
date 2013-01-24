554eef81899c6958297f6b78377eebacd656289f
superatmel <superatmel@gmail.com>
2013-01-24 09:47:53 +0100

    Common:Fix variables
    Change-Id: I18c6bd785cf0d0bfe00dc07070766559cd788a6b

--- config/common.mk
+++ config/common.mk
@@ -53,11 +53,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.error.receiver.system.apps=com.google.android.feedback \
-    ro.com.google.locationfeatures=1 \
     ro.setupwizard.enterprise_mode=1 \
-    windowsmgr.max_events_per_sec=240 \
-    ro.kernel.android.checkjni=0 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
@@ -69,7 +65,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/osr/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/osr/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
-    vendor/osr/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh
+    vendor/osr/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
+    vendor/osr/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
 # init.d support
 PRODUCT_COPY_FILES += \
@@ -80,18 +77,22 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/osr/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
+# CM-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/osr/prebuilt/common/etc/init.local.rc:root/init.cm.rc
+
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
-    vendor/osr/prebuilt/common/etc/init.local.rc:system/etc/init.local.rc \
     vendor/osr/prebuilt/common/bin/compcache:system/bin/compcache \
     vendor/osr/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
 
-# Terminal and Root files
+# Terminal
 PRODUCT_COPY_FILES +=  \
     vendor/osr/proprietary/Term.apk:system/app/Term.apk \
-    vendor/osr/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
-    vendor/osr/prebuilt/common/apps/SuperSU.apk:system/app/SuperSU.apk \
-    vendor/osr/prebuilt/common/xbin/su:system/xbin/su
+    vendor/osr/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+
+#    vendor/osr/prebuilt/common/apps/SuperSU.apk:system/app/SuperSU.apk \
+#    vendor/osr/prebuilt/common/xbin/su:system/xbin/su
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
@@ -123,7 +124,9 @@ PRODUCT_PACKAGES += \
     ROMControl \
     SoundRecorder2 \
     SpareParts \
-    Wallpapers
+    Wallpapers \
+    Superuser \
+    su
 
 # Optional OSR packages
 PRODUCT_PACKAGES += \
@@ -132,21 +135,30 @@ PRODUCT_PACKAGES += \
     SoundRecorder \
     Basic
 
-# Custom C packages
+# Custom OSR packages
 PRODUCT_PACKAGES += \
     Trebuchet \
     DSPManager \
     libcyanogen-dsp \
     audio_effects.conf \
-    Wallpapers \
-    Apollo
+    CMWallpapers \
+    Apollo \
+    CMUpdater \
+    CMFileManager \
+    LockClock
 
 # Extra tools in OSR
 PRODUCT_PACKAGES += \
     openvpn \
     e2fsck \
     mke2fs \
-    tune2fs
+    tune2fs \
+    bash \
+    vim \
+    nano \
+    htop \
+    powertop \
+    lsof
 
 # Openssh
 PRODUCT_PACKAGES += \
@@ -176,4 +188,9 @@ OSR_VERSION := $(shell date -u +%y%m.%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.osr.version=$(PRODUCT_ROM_FILE) \
-  ro.modversion=$(PRODUCT_ROM_FILE)
+  ro.modversion=$(PRODUCT_ROM_FILE) \
+  ro.cm.version=$(CM_VERSION) \
+  ro.modversion=$(CM_VERSION)
+
+
+-include $(WORKSPACE)/hudson/image-auto-bits.mk
