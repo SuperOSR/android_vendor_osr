b5d15df47ea6a1e35957187e1180e2206e4ebef2
superatmel <superatmel@gmail.com>
2013-09-13 15:19:10 +0200

    Eliminamos SoundRecorder

--- mods/terminales/jfltexx/personalizar.sh
+++ mods/terminales/jfltexx/personalizar.sh
@@ -2,4 +2,6 @@
 echo " "
 msgWarn1 "Personalizacion exclusiva para $ALIAS"
 msgStatus " "
+rm -rf $OUT/repack.d/ota/system/app/SoundRecorder.apk
 rm -rf $OUT/repack.d/ota/system/app/SpeechRecorder.apk
+
