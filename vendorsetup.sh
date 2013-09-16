d2338280dcd683d7b3c9f8c17080a5c240ccb8bf
Juan garcia <superatmel@gmail.com>
2013-09-16 23:23:24 +0200

    Add Galaxy S to vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,6 +10,7 @@ fi
 
 # Devices supported
 add_lunch_combo osr_dragon-eng
+add_lunch_combo osr_galaxysmtd-eng
 add_lunch_combo osr_jfltexx-eng
 add_lunch_combo osr_mako-eng
 add_lunch_combo osr_u10z-eng
